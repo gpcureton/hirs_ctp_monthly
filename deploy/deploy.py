40c3fd3d124d9de5faffd2b5a9fa130138a15404
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2015-10-15 20:32:03 +0000

    Change hirs_version package to v20151014. Add logging and traceback support

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,12 +1,16 @@
 from flo_deploy.packagelib import *
 
+# every module should have a LOG object
+import logging, traceback
+LOG = logging.getLogger(__file__)
+
 
 class HIRS_CTP_MONTHLY_Package(Package):
 
     def deploy_package(self):
 
-        for version in ['v20140204']:
-            self.merge(Extracted('HIRS_CTP_MONTHLY_v20140204.tar.gz'.format(version)).path(),
+        for version in ['v20150915']:
+            self.merge(Extracted('HIRS_CTP_MONTHLY_{}.tar.gz'.format(version)).path(),
                        version)
             self.merge(NetcdfFortran().path(), version)
             self.merge(Netcdf().path(), version)
--- source/flo/__init__.py
+++ source/flo/__init__.py
@@ -7,6 +7,10 @@ from flo.time import TimeInterval
 from flo.util import augmented_env, symlink_inputs_to_working_dir
 from flo.sw.hirs_ctp_daily import HIRS_CTP_DAILY
 
+# every module should have a LOG object
+import logging, traceback
+LOG = logging.getLogger(__file__)
+
 
 class HIRS_CTP_MONTHLY(Computation):
 
